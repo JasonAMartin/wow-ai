5857a73c4598a2140dc19acdb1231a446b921c43
JAM <jam@serabyte.com>
2025-02-21 15:19:24 -0800

    ai: MyGearTracker now has spec, level, class and overall item lvl

--- plugins/MyGearTracker/MyGearTracker.lua
+++ plugins/MyGearTracker/MyGearTracker.lua
@@ -105,7 +105,6 @@ local function GetGearInfo()
             local itemLevel = GetDetailedItemLevelInfo(itemLink) or "N/A"
             local upgradeInfo = "N/A"
             
-            -- Scan tooltip for upgrade status
             tooltipScanner:ClearLines()
             tooltipScanner:SetHyperlink(itemLink)
             for i = 1, tooltipScanner:NumLines() do
@@ -145,6 +144,67 @@ local function GetCurrencyInfo()
         end
     end
     return currencyText
+end
+
+-- Get spec name by ID
+function GetSpecNameByID(specID)
+    print("Calling spec list with")
+    print(specID)
+    local specList = {
+        -- Death Knight
+        [250] = "Blood",
+        [251] = "Frost",
+        [252] = "Unholy",
+        -- Demon Hunter
+        [577] = "Havoc",
+        [581] = "Vengeance",
+        -- Druid
+        [102] = "Balance",
+        [103] = "Feral",
+        [104] = "Guardian",
+        [105] = "Restoration",
+        -- Hunter
+        [253] = "Beast Mastery",
+        [254] = "Marksmanship",
+        [255] = "Survival",
+        -- Mage
+        [62] = "Arcane",
+        [63] = "Fire",
+        [64] = "Frost",
+        -- Monk
+        [268] = "Brewmaster",
+        [269] = "Windwalker",
+        [270] = "Mistweaver",
+        -- Paladin
+        [65] = "Holy",
+        [66] = "Protection",
+        [70] = "Retribution",
+        -- Priest
+        [256] = "Discipline",
+        [257] = "Holy",
+        [258] = "Shadow",
+        -- Rogue
+        [259] = "Assassination",
+        [260] = "Outlaw",
+        [261] = "Subtlety",
+        -- Shaman
+        [262] = "Elemental",
+        [263] = "Enhancement",
+        [264] = "Restoration",
+        -- Warlock
+        [265] = "Affliction",
+        [266] = "Demonology",
+        [267] = "Destruction",
+        -- Warrior
+        [71] = "Arms",
+        [72] = "Fury",
+        [73] = "Protection",
+        -- Evoker
+        [1467] = "Devastation",
+        [1468] = "Preservation",
+        [1473] = "Augmentation"
+    }
+    return specID --specList[specID] or "Unknown Spec ID"
 end
 
 -- Update UI
@@ -188,8 +248,14 @@ end
 SLASH_MYGEAREXPORT1 = "/mgtexport"
 SlashCmdList["MYGEAREXPORT"] = function()
     UpdateUI()
+    local overallILvl, equippedILvl = GetAverageItemLevel()
     local jsonData = {
         character = {
+            name = UnitName("player"),
+            class = select(2, UnitClass("player")),
+            spec = GetSpecNameByID(select(2, GetSpecializationInfo(GetSpecialization())) or 0),
+            level = UnitLevel("player"),
+            overallItemLevel = math.floor(equippedILvl),
             gear = {},
             currencies = exportData.currencies
         }
